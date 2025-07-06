af069443aaf75a1797469900c380d79c760040aa
Max Maciag <maciagmax1@gmail.com>
2025-07-06 14:14:27 -0400

    resolve one unused import

--- studio/src/schemaTypes/index.ts
+++ studio/src/schemaTypes/index.ts
@@ -1,8 +1,7 @@
 import blockContent from './objects/blockContent'
-import post from './documents/post'
 import project from './documents/project'
 import {author} from './documents/types'
 
 // Export an array of all the schema types.  This is used in the Sanity Studio configuration. https://www.sanity.io/docs/schema-types
 
-export const schemaTypes = [post, blockContent, project, author]
+export const schemaTypes = [blockContent, project, author]
