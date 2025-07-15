f0bffd82565d7619979ea572685ceeb174da0b18
Max Maciag <maciagmax1@gmail.com>
2025-07-14 23:56:56 -0400

    feat: add landing page schema and implement related components

---
+++ studio/src/schemaTypes/documents/landingPage.ts
@@ -0,0 +1,27 @@
+import {defineType} from 'sanity'
+
+export default defineType({
+  name: 'landingPage',
+  title: 'Landing Page',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    },
+    {
+      name: 'content',
+      title: 'Content',
+      type: 'blockContent',
+    },
+    {
+      name: 'mainImage',
+      title: 'Main Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+  ],
+})
--- studio/src/schemaTypes/index.ts
+++ studio/src/schemaTypes/index.ts
@@ -2,8 +2,8 @@ import blockContent from './objects/blockContent'
 import project from './documents/project'
 import author from './objects/author'
 import navigation from './documents/navigation'
-import page from './documents/page'
+import landingPage from './documents/landingPage'
 
 // Export an array of all the schema types.  This is used in the Sanity Studio configuration. https://www.sanity.io/docs/schema-types
 
-export const schemaTypes = [blockContent, author, navigation, project, page]
+export const schemaTypes = [blockContent, author, navigation, project, landingPage]
