fb17b8763d6a6ac5054d04910e4f8ab2d2f9b764
Max Maciag <maciagmax1@gmail.com>
2025-07-24 22:24:22 -0400

    feat: enhance Carousel component with dynamic slide display options and update related imports

--- astro-app/src/components/Carousel.tsx
+++ astro-app/src/components/Carousel.tsx
@@ -6,10 +6,13 @@ import {
   CarouselPrevious,
   Carousel as ShadCnCarousel,
 } from "@/components/ui/carousel";
+import { cn } from "@/lib/utils";
 import type { ImageAsset } from "sanity";
 
 export function Carousel({
   carouselItems,
+  options,
+  slidesToShow = 2,
 }: {
   carouselItems?: {
     label: string;
@@ -17,19 +20,28 @@ export function Carousel({
     image: ImageAsset;
     url: string;
   }[];
+  options?: React.ComponentProps<typeof ShadCnCarousel>["opts"];
+  slidesToShow?: number;
 }) {
   return (
-    <ShadCnCarousel
-      className="w-full max-w-xs"
-      opts={{
-        slidesToScroll: 1,
-        loop: true,
-        dragFree: true,
-      }}
-    >
+    <ShadCnCarousel className="w-full max-w-sm" opts={options}>
       <CarouselContent>
         {carouselItems?.map((item) => (
-          <CarouselItem key={item.label} className="">
+          <CarouselItem
+            key={item.label}
+            className={cn(
+              "min-w-0 shrink-0 grow-0",
+              slidesToShow === 1
+                ? "basis-full"
+                : slidesToShow === 2
+                  ? "basis-1/2"
+                  : slidesToShow === 3
+                    ? "basis-1/3"
+                    : slidesToShow === 4
+                      ? "basis-1/4"
+                      : "basis-full",
+            )}
+          >
             <div className="p-1">
               <Card>
                 <img src={item.image.url} />
